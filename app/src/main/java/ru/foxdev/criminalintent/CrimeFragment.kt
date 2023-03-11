1382c55e0edfe32f4f6673c7f0986a3445c286fb
Alexander Gavrilyuk <one1970zero@mail.ru>
2023-03-11 05:15:45 +0300

    Добавление новых функций

--- app/src/main/java/ru/foxdev/criminalintent/CrimeFragment.kt
+++ app/src/main/java/ru/foxdev/criminalintent/CrimeFragment.kt
@@ -1,6 +1,55 @@
 package ru.foxdev.criminalintent
 
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.EditText
 import androidx.fragment.app.Fragment
 
 class CrimeFragment:Fragment() {
+    private lateinit var crime: Crime
+    private lateinit var titleField: EditText
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        crime = Crime()
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_crime, container, false)
+        titleField = view.findViewById(R.id.crime_title) as EditText
+        return view
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val titleWatcher = object : TextWatcher {
+
+            override fun beforeTextChanged(
+                sequence: CharSequence?,
+                start: Int,
+                coutn: Int,
+                after: Int
+            ) {}
+
+            override fun onTextChangd(
+                sequence: CharSequence?,
+                start: Int,
+                coutn: Int,
+                after: Int
+            ) {
+                crime.title = sequence.toString()
+            }
+
+            override fun afterTextChanged(sequence: Editable?) {}
+        }
+
+        titleField.addTextChangedListener(titleWatcher)
+    }
 }
